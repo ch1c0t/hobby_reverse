788513ed74deaebf9c4a478fb76b20de4d4391fa
Anatoly Chernow <chertoly@gmail.com>
2017-03-18 23:45:24 +0200

    Experiment on how to integrate Mutant with Testo

--- spec_with_testo_and_rack-test/run.rb
+++ spec_with_testo_and_rack-test/run.rb
@@ -17,10 +17,64 @@ test = Test.new do
   json = JSON.parse last_response.body
 
   #json['id'] == 'App' && json['text'] == 'txet'
-  json['id'] == 'WrongAssertion' && json['text'] == 'txet'
+  #json['id'] == 'WrongAssertion' && json['text'] == 'txet'
+  json['id'] == 'App' # passing but not covering App#replace to let some mutants stay alive
 end
 
-require 'pry'
-report = test[:anything]
-binding.pry
-report.debug
+if ENV['MUTANT']
+  require 'mutant'
+
+  class Mutant::Selector::Expression
+    def call _subject
+      integration.all_tests
+    end
+  end
+
+  module Integration
+    extend self
+
+    def all_tests
+    end
+
+    def call tests
+    end
+
+    def new _config
+      self
+    end
+
+    def setup
+      self
+    end
+  end
+
+  namespace = App.name
+  c = Mutant::Config::DEFAULT
+  config = c.with \
+    matcher: c.matcher.add(:match_expression, c.expression_parser.(namespace)),
+    integration: Integration
+  env = Mutant::Env::Bootstrap.call config
+
+  puts "{env.mutatitions.size} mutations are to be checked."
+  alive_mutants = env.mutations.map.with_index 1 do |mutation, index|
+    puts index
+    env.kill mutation
+  end.reject &:success?
+
+  if alive_mutants.empty?
+    puts "No alive mutants were found."
+  else
+    require 'pry'
+    alive_mutants.__binding__.pry
+  end
+else
+  report = test[:anything]
+
+  if report.ok?
+    puts 'All passed.'
+  else
+    require 'pry'
+    binding.pry
+    report.debug
+  end
+end
